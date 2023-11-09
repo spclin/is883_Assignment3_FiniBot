4a7f0f057b1cdb83b37b1ecfddd5205f5c4ce0d0
Mohannad Elhamod <mndhamod@gmail.com>
2023-11-08 20:46:10 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+# Import necessary libraries
+import streamlit as st
+import openai
+import os
+from io import StringIO
+import pandas as pd
+
+
+
+# Make sure to add your OpenAI API key in the advanced settings of streamlit's deployment
+open_AI_key = os.environ.get('OPENAI_API_KEY')
+openai.api_key = open_AI_key
+
+
+### Here, with some adjustments, copy-paste the code you developed for Question 1 in Assignment 3 
+##########################################################################
+
+
+##########################################################################
+
+
+# UX goes here. You will have to encorporate some variables from the code above and make some tweaks.
+
+
+
